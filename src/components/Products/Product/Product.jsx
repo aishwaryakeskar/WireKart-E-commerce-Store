b1a4422e15bdc2ae6bbac338807cf7b2afafab38
Aishwarya Keskar <aishwaryakeskar98@gmail.com>
2021-12-09 22:54:02 +0530

    Add commerce.js api

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -7,19 +7,23 @@ import useStyles from './styles';
 const Product = ({ product }) => {
     const classes = useStyles();
 
+    // console.log(product);
+
+    // return <div>test</div>
+
     return (
         <Card className={classes.root}>
-            <CardMedia className={classes.media} image={product.image} title={product.name} />
+            <CardMedia className={classes.media} image={product.image.url} title={product.name} />
             <CardContent>
             <div className={classes.cardContent}>
                 <Typography gutterBottom variant="h5">
                     {product.name}
                 </Typography>
                 <Typography gutterBottom variant="h5">
-                    {product.price}
+                    {product.price.formatted_with_symbol}
                 </Typography>
             </div>
-            <Typography variant="body2" color="textSecondary">{product.description}</Typography>
+            <Typography dangerouslySetInnerHTML={{__html:product.description }} variant="body2" color="textSecondary"/>
             </CardContent>
             <CardActions disableSpacing className={classes.cardActions}>
                 <IconButton aria-label="Add to Cart">
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,13 +4,9 @@ import { Grid } from '@material-ui/core';
 import Product from './Product/Product';
 import useStyles from './styles';
 
-const products = [
-    {id: 1, name: 'Shoes', description: 'Running shoes.', price: '$10', image: 'https://m.media-amazon.com/images/I/41Leu3gBUFL.jpg'},
-    {id: 2, name: 'Macbook', description: 'Apple laptop.', price: '$50', image: 'https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/macbook-pro-13-og-202011?wid=600&hei=315&fmt=jpeg&qlt=95&.v=1604347427000'},
-    {id: 3, name: 'Headphones', description: 'Bluetooth headphones.', price: '$15', image: 'https://adn-static1.nykaa.com/nykdesignstudio-images/tr:w-824,/pub/media/catalog/product/j/b/jbl_jbllive500btblu_1.jpg?rnd=20200526195200'}
-];
 
-const Products = () => {
+
+const Products = ({ products }) => {
     const classes = useStyles();
 
     return (
